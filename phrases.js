2a703ee249aa7390e7e30bf1d3c162c07fe7f8ba
asyagetmanova <asya.getmanova00@gmail.com>
2026-03-05 13:05:03 +0700

    Update phrases.js

--- phrases.js
+++ phrases.js
@@ -1,1 +1,8 @@
-
+window.PHRASES = [
+  "You make my life warmer",
+  "I feel safe with you",
+  "I love building our life together",
+  "You are my favorite person",
+  "I’m proud of you",
+  "You make ordinary days beautiful"
+];
